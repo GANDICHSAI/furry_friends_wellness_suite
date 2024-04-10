f55d762605419349531c4fcbe209588710af7dc5
haneeshraj <haneeshraj.banisetti@gmail.com>
2024-04-10 17:57:51 -0400

    feat: create and delete store

--- src/Views/systemAdmin/ManageStores.java
+++ src/Views/systemAdmin/ManageStores.java
@@ -4,22 +4,63 @@
  */
 package Views.systemAdmin;
 
+import Models.ClientInformationManager;
+import Models.Store;
+import Utilities.SystemAdminController;
 import java.awt.CardLayout;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author hanee
  */
-public class ManageStores extends javax.swing.JPanel {
+public final class ManageStores extends javax.swing.JPanel {
 
     /**
      * Creates new form ManageStoress
      */
     JPanel bottomPanel;
+    private Store selectedStore;
+    private Store editingStore;
+    private ArrayList<Store> stores;
+    
     public ManageStores(JPanel bottomPanel) {
         initComponents();
         this.bottomPanel = bottomPanel;
+        populateTable();
+    }
+    
+    private void clearFields() {
+        
+        storeNameInput.setText("");
+        storePostalInput.setText("");
+        idValue.setText("");
+        
+        selectedStore = null;
+        editingStore = null;
+    }
+    
+    public void populateTable() {
+        try {
+            
+            this.stores = SystemAdminController.getAllStores();
+            DefaultTableModel tableModel = (DefaultTableModel) jTable1.getModel();
+            
+            tableModel.setRowCount(0);
+            
+            for (Store store : stores) {
+                String[] storeData = {(String) Integer.toString(store.getStoreId()), store.getStoreName(), store.getPostalCode()};
+                tableModel.addRow(storeData);
+            }
+            
+            clearFields();
+            
+        } catch (Exception e) {
+            
+        }
     }
 
     /**
@@ -67,10 +108,25 @@ public class ManageStores extends javax.swing.JPanel {
         table.setViewportView(jTable1);
 
         addStoreBtn.setText("Add Store");
+        addStoreBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addStoreBtnActionPerformed(evt);
+            }
+        });
 
         saveChangesBtn.setText("Save changes");
+        saveChangesBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                saveChangesBtnActionPerformed(evt);
+            }
+        });
 
         editStoreBtn.setText("Edit Store");
+        editStoreBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                editStoreBtnActionPerformed(evt);
+            }
+        });
 
         idLabel.setForeground(new java.awt.Color(255, 255, 255));
         idLabel.setText("Store ID");
@@ -80,6 +136,11 @@ public class ManageStores extends javax.swing.JPanel {
         idValue.setText("abc1234");
 
         deleteStoreBtn.setText("Delete Store");
+        deleteStoreBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                deleteStoreBtnActionPerformed(evt);
+            }
+        });
 
         storeNameLabel.setForeground(new java.awt.Color(255, 255, 255));
         storeNameLabel.setText("Store name");
@@ -181,6 +242,105 @@ public class ManageStores extends javax.swing.JPanel {
         CardLayout layout = (CardLayout) bottomPanel.getLayout();
         layout.next(bottomPanel);
     }//GEN-LAST:event_backToMenuBtnActionPerformed
+
+    private void addStoreBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addStoreBtnActionPerformed
+        // TODO add your handling code here:
+        
+        if(storeNameInput.getText().equals("") || storePostalInput.getText().equals("")) {
+            JOptionPane.showMessageDialog(null, "Please make sure you fill all the fields!", "Empty data inputs", JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+        
+        Store newStore = new Store();
+        
+        newStore.setPostalCode(storePostalInput.getText());
+        newStore.setStoreName(storeNameInput.getText());
+        
+        SystemAdminController.addStore(newStore);
+        
+       populateTable();
+        
+    }//GEN-LAST:event_addStoreBtnActionPerformed
+
+    private void editStoreBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editStoreBtnActionPerformed
+        // TODO add your handling code here:
+        
+        try {
+            int selectedRowIndex = jTable1.getSelectedRow();
+            if (selectedRowIndex < 0) {
+                throw new IllegalArgumentException("Please select a store you want to edit the data for!");
+            } else {
+                editingStore = stores.get(selectedRowIndex);
+                idValue.setText((String) Integer.toString(editingStore.getStoreId()));
+                storeNameInput.setText(editingStore.getStoreName());
+                storePostalInput.setText(editingStore.getPostalCode());
+            }
+        } catch (IllegalArgumentException e) {
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Selection Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }//GEN-LAST:event_editStoreBtnActionPerformed
+
+    private void saveChangesBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveChangesBtnActionPerformed
+        // TODO add your handling code here:
+        try {
+            if (editingStore == null) {
+                 throw new IllegalArgumentException("no-select");
+            } else {
+                Store newStore = new Store();
+                
+                newStore.setStoreName(storeNameInput.getText());
+                newStore.setPostalCode(storePostalInput.getText());
+                
+                
+                if (editingStore.getStoreName().equals(newStore.getStoreName()) && editingStore.getPostalCode().equals(newStore.getPostalCode())) {
+                    throw new IllegalArgumentException("no-edit");
+                } else {
+                    SystemAdminController.editStore(editingStore, newStore);
+                    clearFields();
+                    populateTable();
+                    JOptionPane.showMessageDialog(null, "Store succesfully updated!", "Successfully Updated", JOptionPane.INFORMATION_MESSAGE);
+                }
+                
+            }
+        } catch (IllegalArgumentException e) {
+            if (e.getMessage().equals("no-edit")) {
+                JOptionPane.showMessageDialog(this, "Please make some changes to the user you have selected!", "Data Updation Error", JOptionPane.ERROR_MESSAGE);
+            }
+            
+            if (e.getMessage().equals("no-select")) {
+                JOptionPane.showMessageDialog(this, "Please make some you selected a user to edit!", "Data Updation Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }//GEN-LAST:event_saveChangesBtnActionPerformed
+
+    private void deleteStoreBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteStoreBtnActionPerformed
+        // TODO add your handling code here:
+        
+        try {
+            int selectedRowIndex = jTable1.getSelectedRow();
+            if (selectedRowIndex < 0) {
+                
+                throw new IllegalArgumentException("Please select a user to delete!");
+                
+            } else {
+                
+                DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
+                selectedStore = stores.get(selectedRowIndex);
+                SystemAdminController.deleteStore(selectedStore);
+                clearFields();
+                populateTable();
+                JOptionPane.showMessageDialog(null, "Store succesfully deleted!", "Successfully Deleted", JOptionPane.INFORMATION_MESSAGE);
+                
+            }
+            
+        } catch (IllegalArgumentException e) {
+            
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Data Selection Error", JOptionPane.ERROR_MESSAGE);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Data Deletion Error", JOptionPane.ERROR_MESSAGE);
+            
+        }
+    }//GEN-LAST:event_deleteStoreBtnActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
