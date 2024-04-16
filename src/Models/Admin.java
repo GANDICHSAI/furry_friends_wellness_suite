e1d8b2418330d20c908680620ed833664856254a
schoolLychee <li.yihan2@northeastern.edu>
2024-04-16 14:36:05 -0400

    [yihan]implement-inheritance

---
+++ src/Models/Admin.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author yihan
+ */
+public class Admin {
+    private String email;
+    private String password;
+
+    public Admin(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
